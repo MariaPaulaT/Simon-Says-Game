1559fc85dff99c777190ac8e71fc6e7262c2a4d0
MariaPaulaT <mp.tellez@uniandes.edu.co>
2023-02-08 20:28:37 -0500

    making the buttons sound

--- game.js
+++ game.js
@@ -5,11 +5,17 @@ var gamePattern = [];
 
 var userClickedPattern =[];
 
-$("#btn").click(function(){
-    var userChosenColour = this.attr("id");
-    gamePattern.push();
+$(".btn").click(function(){
+    var userChosenColour = $(this).attr("id");
+
+   userClickedPattern.push(userChosenColour);
+   playSound(userChosenColour);
 });
 
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
 function nextSequence(){
 
     var randomNumber = Math.floor(Math.random() * 4);
@@ -20,7 +26,6 @@ function nextSequence(){
   //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
-  var audio = new Audio("sounds/" + randomChosenColour + ".mp3");
-  audio.play();
+  playSound(randomChosenColour);
 }
 
