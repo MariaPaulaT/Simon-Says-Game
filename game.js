07d8e50f228ba62f4b5b3ae3c38616341c7b1ee7
MariaPaulaT <mp.tellez@uniandes.edu.co>
2023-02-08 20:56:13 -0500

    Changes the bottom when its clicked

--- game.js
+++ game.js
@@ -10,22 +10,27 @@ $(".btn").click(function(){
 
    userClickedPattern.push(userChosenColour);
    playSound(userChosenColour);
+   animatedPress(userChosenColour);
 });
 
-function playSound(name){
-    var audio = new Audio("sounds/" + name + ".mp3");
-    audio.play();
-}
 function nextSequence(){
 
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
  
-  //1. Use jQuery to select the button with the same id as the randomChosenColour
-  //2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
   $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
 
   playSound(randomChosenColour);
 }
 
+function animatedPress(currentColor){
+
+    $("#"+currentColor).addClass("pressed");
+    
+    setTimeout ( function(){$("#"+currentColor).removeClass("pressed");}, 100);
+}
+function playSound(name){
+    var audio = new Audio("sounds/" + name + ".mp3");
+    audio.play();
+}
